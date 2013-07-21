e9def4d304234c34a008fa17c1030c5c277f2daf
Yann Lambret <yann.lambret@gmail.com>
2013-07-21 16:31:58 +0200

    Add a test for the throughput attribute.

--- flstats/flstats_tests.py
+++ flstats/flstats_tests.py
@@ -24,7 +24,6 @@ class FlstatsTestCase(unittest.TestCase):
         self.app = Flask(__name__)
         self.app.register_blueprint(webstatistics)
         self.client = self.app.test_client()
-        self.urls = ['http://localhost/url1', 'http://localhost/url2']
 
         @self.app.route('/url1')
         @statistics
@@ -37,49 +36,63 @@ class FlstatsTestCase(unittest.TestCase):
             return random.choice(range(0, 1000))
 
     def test_url1(self):
-        """Sends a unique request to one URL and tests the
-        returned statistics.
-        """
+        """Test with one URL only"""
 
         self.client.get('/url1')
 
         response = self.client.get('/flstats/')
-        data = json.loads(response.data)
-        stats = data['stats']
-
-        # /flstats access tests
         self.assertEqual(response.status, '200 OK')
 
         # Statistics tests
+        data = json.loads(response.data)
+        stats = data['stats']
         self.assertEqual(len(stats), 1)
         stat = stats.pop()
         self.assertEqual(stat['url'], 'http://localhost/url1')
         self.assertEqual(stat['count'], 1)
+        self.assertEqual(stat['throughput'], 1)
         self.assertTrue(stat['min'] == stat['avg'] == stat['max'])
 
-    def test_url2(self):
-        """Sends requests to both URLs and tests the
-        returned statistics.
-        """
-
-        for i in range(1, 10):
+        for i in range(0, 9):
             self.client.get('/url1')
-        for i in range(0, 10):
+
+        response = self.client.get('/flstats/')
+        self.assertEqual(response.status, '200 OK')
+ 
+        # Statistics tests
+        data = json.loads(response.data)
+        stats = data['stats']
+        self.assertEqual(len(stats), 1)
+        stat = stats.pop()
+        self.assertEqual(stat['url'], 'http://localhost/url1')
+        self.assertEqual(stat['count'], 10)
+        self.assertEqual(stat['throughput'], 9)
+        self.assertTrue(stat['min'] <= stat['avg'] <= stat['max'])
+
+    def test_url2(self):
+        """Test with two URLs"""
+
+        for i in range(0, 20):
             self.client.get('/url2')
 
         response = self.client.get('/flstats/')
-        data = json.loads(response.data)
-        stats = data['stats']
-
-        # /flstats access tests
         self.assertEqual(response.status, '200 OK')
 
         # Statistics tests
+        data = json.loads(response.data)
+        stats = data['stats']
         self.assertEqual(len(stats), 2)
         for stat in stats:
-            self.assertTrue(stat['url'] in self.urls)
-            self.assertEqual(stat['count'], 10)
-            self.assertTrue(stat['min'] <= stat['avg'] <= stat['max'])
+            if stat['url'] == 'http://localhost/url1':
+                self.assertEqual(stat['count'], 10)
+                self.assertEqual(stat['throughput'], 0)
+                self.assertTrue(stat['min'] <= stat['avg'] <= stat['max'])
+            elif stat['url'] == 'http://localhost/url2':
+                self.assertEqual(stat['count'], 20)
+                self.assertEqual(stat['throughput'], 20)
+                self.assertTrue(stat['min'] <= stat['avg'] <= stat['max'])
+            else:
+                self.fail('Invalid URL, WTF?!')
 
 if __name__ == '__main__':
     unittest.main()
